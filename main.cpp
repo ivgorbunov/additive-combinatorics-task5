7681ed12d2c5513c68d3fc820deeea6053529fd8
Ivgorbunov <ivgorbunov@me.com>
2026-03-15 21:56:43 +0300

    Bruteforce

--- main.cpp
+++ main.cpp
@@ -1,16 +1,64 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <set>
 
+int get_add(std::vector<int> &a, bool print = false) {
+    std::set<int> b;
+    for (auto v : a) {
+        for (auto u : a) {
+            b.insert(v + u);
+        }
+    }
+    if (print) {
+        for (auto v : b) {
+            std::cout << v << ", ";
+        }
+    }
+    return b.size();
+}
+
+int get_sub(std::vector<int> &a, bool print = false) {
+    std::set<int> b;
+    for (auto v : a) {
+        for (auto u : a) {
+            b.insert(v - u);
+        }
+    }
+    if (print) {
+        for (auto v : b) {
+            std::cout << v << ", ";
+        }
+    }
+    return b.size();
+}
+
+std::vector<int> arr;
+
+void gen(int n) {
+    if (n == -1) {
+        if (get_add(arr) > get_sub(arr)) {
+            std::cout << "Gotcha!\n";
+            std::cout << "$$A = ";
+            std::reverse(arr.begin(), arr.end());
+            for (auto v : arr) {
+                std::cout << v << ", ";
+            }
+            std::cout << "$$\n";
+            std::cout << "$$A + A = " << "\\{ "; get_add(arr, true); std::cout << "\\} $$\n";
+            std::cout << "$$|A + A| = " << get_add(arr, false) << "$$\n";
+            std::cout << "$$A - A = " << "\\{ "; get_sub(arr, true); std::cout << "\\} $$\n";
+            std::cout << "$$|A - A| = " << get_sub(arr, false) << "$$\n";
+            exit(0);
+        }
+        return;
+    }
+    gen(n - 1);
+    arr.push_back(n);
+    gen(n - 1);
+    arr.pop_back();
+}
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    gen(20);
 }
