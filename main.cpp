a15ba2630b1ee146ff61045ba8c9b02151700ed1
Ivgorbunov <ivgorbunov@me.com>
2026-03-15 23:07:26 +0300

    Drop CLion comment

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ void gen(int n) {
     gen(n - 1);
     arr.pop_back();
 }
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 int main() {
     gen(20);
 }
